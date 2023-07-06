d5c35ca189b2354e592b98f470473f1b8341da48
Vivek Gandharkar <vivekgandharkar@outlook.com>
2023-07-06 21:10:42 +0530

    feat: add healthcheck endpoint

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World.")
+	app := fiber.New()
+
+	app.Get("healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Fatal(app.Listen(":8080"))
 }
